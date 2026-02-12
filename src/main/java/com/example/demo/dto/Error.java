817f308692f04c0b9a20e478cfa98107d2b47693
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-12 16:40:06 -0300

    feat: Class Error

--- src/main/java/com/example/demo/dto/Error.java
+++ src/main/java/com/example/demo/dto/Error.java
@@ -4,4 +4,35 @@ import java.time.LocalDateTime;
 
 public class Error {
     private LocalDateTime dataHora;
+    private int status;
+    private String mensage;
+
+    public Error(int status, String mensage) {
+        this.status = status;
+        this.mensage = mensage;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMensage() {
+        return mensage;
+    }
+
+    public void setMensage(String mensage) {
+        this.mensage = mensage;
+    }
 }
